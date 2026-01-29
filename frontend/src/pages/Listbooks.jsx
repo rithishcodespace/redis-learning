06fd7fbdd923d3fa9ed3a3d94b73021d97e68e71
rithishcodespace <rithishcodespace@gmail.com>
2026-01-29 21:15:31 +0530

    pages are added

--- frontend/src/pages/Listbooks.jsx
+++ frontend/src/pages/Listbooks.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+useEffect(() => {
+    fetchBooks();
+},[])
+
+const fetchBooks = async () => {
+    let response = await fetch()
+}
+
 const Listbook = () => {
     return(
-        <div>
-            list book
+        <div className="flex justify-center align-middle m-40">
+            <div className="h-125 w-2/4 bg-amber-300">
+               
+            </div>
         </div>
     )
 }
