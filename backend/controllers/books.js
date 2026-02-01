22e1a0521c96a76f42f6f0dde717896c8fd42171
rithishcodespace <rithishcodespace@gmail.com>
2026-02-01 18:55:11 +0530

    added api's for update and delete

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -39,3 +39,83 @@ exports.create_book = async(req,res,next) => {
         next(error);
     }
 }
+
+exports.update_book = async (req, res, next) => {
+  try {
+    const { id, name, author, rating, description } = req.body;
+
+    if (!id) {
+      return next(createError.NotFound("id not found!"));
+    }
+
+    if (!name?.trim() || !author?.trim() || !rating || !description?.trim()) {
+      return next(createError.BadRequest("Invalid payload!"));
+    }
+
+    let books = await redis.lRange("books", 0, -1);
+
+    let updated = false;
+
+    books = books.map((bookStr) => {
+      const book = JSON.parse(bookStr);
+
+      if (book.id === id) {
+        updated = true;
+        return JSON.stringify({
+          ...book,
+          name,
+          author,
+          rating,
+          description
+        });
+      }
+
+      return bookStr;
+    });
+
+    if (!updated) {
+      return next(createError.NotFound("Book not found"));
+    }
+
+    await redis.del("books");
+    await redis.rPush("books", books);
+
+    res.status(200).json({
+      message: "Book updated successfully"
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.delete_book = async(req,res,next) => {
+    try{
+        const{id} = req.body;
+        if(!id){
+            return next(createError.NotFound("id not found!"));
+        }
+        let books = await redis.lRange("books",0,-1);
+        let updated = false;
+        books = books.filter((book) => {
+            book = json.parse();
+            if(book.id === id){
+                updated = true;
+                return false;
+            }
+            return true;
+        })
+        if(!updated){
+            return res.status(200).json({
+                "message":"book not found"
+            })
+        }
+        await redis.del("books");
+        if (books.length > 0) {
+            await redis.rPush("books", books);
+        }
+    }
+    catch(error){
+        next(error);
+    }
+}
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -4,6 +4,7 @@ const books = require("../controllers/books");
 
 router.get("/api/getBooks",books.getBooks);
 router.post("/api/createBook",books.create_book);
-
+router.delete("/api/delete_book",books.delete_book);
+router.patch("/api/update_book",books.update_book);
 
 module.exports = router;
